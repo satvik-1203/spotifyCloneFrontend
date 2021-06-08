918204c8a8f254dfe94fb5dbadc7f0720082796d
Satvik Seeram <satvikgsu@gmail.com>
2021-06-08 08:28:02 +0530

    bug

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -3,4 +3,4 @@ const BASE_URL = "http://localhost:3001/spotify/";
 export const SearchURL = BASE_URL + "search";
 export const LoginUrl = BASE_URL + "login";
 export const userInfoUrl = BASE_URL + "userInfo";
-export const loginWithToken = BASE_URL + "loginWithToken";
+export const loginWithToken = BASE_URL + "loginToken";
