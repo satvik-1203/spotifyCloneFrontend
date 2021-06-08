313a4dee2b2b8baf500e880d313815512b3e2e42
Satvik Seeram <satvikgsu@gmail.com>
2021-06-08 21:46:05 +0530

    final

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -4,3 +4,5 @@ export const SearchURL = BASE_URL + "search";
 export const LoginUrl = BASE_URL + "login";
 export const userInfoUrl = (accessToken) =>
   BASE_URL + "userInfo/" + accessToken;
+
+export const loginWithToken = (token) => `${BASE_URL}loginToken/${token}`;
